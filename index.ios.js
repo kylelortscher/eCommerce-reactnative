0151735c64272fe19761a23d637abd73410cfdac
Kyle Lortscher <kylelortscher@Kyles-MBP-2.attlocal.net>
2016-05-26 18:59:59 -0700

    Added view and text

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,8 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
 
 class eCommerce extends Component {
@@ -32,6 +33,10 @@ class eCommerce extends Component {
         </Text>
         {this.emailInput()}
         {this.passwordInput()}
+        {this.loginButton()}
+        {this.linkToSignUp()}
+        {this.linkToForgotPassword()}
+        {this.linkToBrowse()}
       </View>
     );
   }
@@ -42,10 +47,37 @@ class eCommerce extends Component {
       </TextInput>
     );
   }
+  //Password
   passwordInput() {
     return(
       <TextInput style={styles.textInput} value={this.state.email} placeholder="  Password">
       </TextInput>
+    );
+  }
+  //Login Button
+  loginButton() {
+    return(
+      <TouchableHighlight style={styles.loginButton}>
+        <Text style={styles.loginButtonText}>Log In</Text>
+      </TouchableHighlight>
+    );
+  }
+  //Link To Signup
+  linkToSignUp() {
+    return(
+      <Text style={styles.linkToBrowse}>Sign Up!</Text>
+    );
+  }
+  //Link To ForgotPassword
+  linkToForgotPassword() {
+    return(
+      <Text style={styles.linkToBrowse}>Forgot Password</Text>
+    );
+  }
+  //Link To Browse
+  linkToBrowse() {
+    return(
+      <Text style={styles.linkToBrowse}>Just Browse</Text>
     );
   }
 }
@@ -68,6 +100,26 @@ const styles = StyleSheet.create({
     margin: 10,
     borderRadius: 5,
     backgroundColor: 'white'
+  },
+  loginButton: {
+    height: 50,
+    width: 150,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50'
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 16,
+    alignItems: 'center'
+  },
+  linkToBrowse: {
+    fontSize: 13,
+    textDecorationLine: 'underline',
+    color: 'white',
+    alignItems: 'center',
+    marginTop: 10
   },
   welcome: {
     fontSize: 20,
